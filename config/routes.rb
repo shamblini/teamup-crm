37f4d766c338385bfecf71abf11f295704e10912
shamblini <78273476+shamblini@users.noreply.github.com>
2024-02-07 17:11:34 -0600

    undo routes change

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  root '/books#index'
+  root 'books#index'
 
-  devise_for :admins, controllers: { omniauth_callbacks: '/admins/omniauth_callbacks' }
+  devise_for :admins, controllers: { omniauth_callbacks: 'admins/omniauth_callbacks' }
   devise_scope :admin do
-    get '/admins/sign_in', to: '/admins/sessions#new', as: :new_admin_session
-    get '/admins/sign_out', to: '/admins/sessions#destroy', as: :destroy_admin_session
+    get 'admins/sign_in', to: 'admins/sessions#new', as: :new_admin_session
+    get 'admins/sign_out', to: 'admins/sessions#destroy', as: :destroy_admin_session
   end
 
   resources :books
