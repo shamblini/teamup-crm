374ac71c3baa6761111d223729dda8d1c9e47908
jonnguyenle087 <jonnguyenle087@tamu.edu>
2024-02-09 16:14:51 -0600

    Fixing typos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_08_192242) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_09_145017) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,16 +24,16 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_08_192242) do
     t.index ["email"], name: "index_admins_on_email", unique: true
   end
 
+  create_table "donations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "org_type"
-  end
-
-  create_table "donations", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "logs", force: :cascade do |t|
