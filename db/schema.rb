f3543a619df23956e5ddcb348e25a6f47ba05c2d
root <root@localhost>
2024-02-09 15:20:18 +0000

    commit CRUD changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_08_195714) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_09_145017) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_08_195714) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "org_type"
   end
 
 end
