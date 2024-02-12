c62b7b9bf7b8b50737c85f4a632174443883cf6f
Dallasocoggins <dallasocoggins@gmail.com>
2024-02-12 15:15:11 -0600

    Admin only and button to view users
    Only allows admins who are in the database to login (previously anyone could login regardless of whether they were an admin)
    
    Also added a button to go to the user views from the admin dashboard.
    
    Also changed the root of the website from the books view to the dashboard view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   belongs_to :donations, class_name: 'Donation', optional: true
   belongs_to :logs, class_name: 'Log', optional: true
+  USER_CREATE = 1
+  USER_READ = 2
+  USER_UPDATE = 4
+  USER_DELETE = 8
 end
