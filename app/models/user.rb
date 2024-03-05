5ea537873204a5f34e622ded3bc9c1916d75a788
Dallasocoggins <dallasocoggins@gmail.com>
2024-03-04 20:18:25 -0600

    user model fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   def self.from_google(email:, full_name:, uid:, avatar_url:)
     # return nil unless email =~ /@mybusiness.com\z/
     create_with(uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
+  end
 
   has_many :donations
   def calculate_total_donations
